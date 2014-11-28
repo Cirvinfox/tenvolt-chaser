7fd7f1277076e003cb0eae7cd0b0906c7116aad7
Olivia Theze <fox@foxiepa.ws>
2014-11-28 05:52:07 +0000

    code and comment style enforcement.
    changelog will be added in next commit

--- chaser.c
+++ chaser.c
@@ -1,24 +1,34 @@
-/* Corbin Irvin 2011
- 
- Changelog:
- 10/26/2012 - CA Irvin
- Removed sequence properties
- Added serial routines
+/* 
+ * Corbin Irvin 2011
+ * 
+ * Please see LICENSE for License details
+ *
+ * Changelog:
+ * 2014-11-28 - Olivia Theze
+ *   - Normalised code style and comments.
+ *   - Moved function prototypes and defines to header instead
+ *   - Added Vim modeline 
+ *   - Fixed Changelog format
+ *
+ * 2012-10-26 - CA Irvin
+ *   - Removed sequence properties
+ *   - Added serial routines
  */
-//#include <msp430g2221.h>  
+
+/* #include <msp430g2221.h> */  
 #include <msp430.h>
 #include <signal.h>
 #include <inttypes.h>
 #include "chaser.h"
 
 /* global vars */
-volatile uint32_t regTimer = 0;	// timer register, starts at 0 on startup, 500uS steps
+volatile uint32_t   regTimer = 0;	 /* timer register, starts at 0 on startup, 500uS steps */
 volatile uint8_t	keyRegOld;
-volatile uint8_t	keyReg;		// register that holds key state
+volatile uint8_t	keyReg;          /* register that holds key state */
 volatile uint8_t	keyPulse;
 
 volatile int		tapState =0;
-volatile uint32_t delayTime = 500; // delay time in half milliseconds
+volatile uint32_t   delayTime = 500; /* delay time in half milliseconds */
 
 void main(void)
 {
@@ -26,20 +36,20 @@ void main(void)
 	int i =0;
 	
 	/* watchdog timer is pwm timebase */
-	WDTCTL = WDTPW | WDT_MDLY_0_5;			// Set up watchdog to exp, 0.5ms
-	IE1 |= WDTIE;							// enable wtd interrupt
+	WDTCTL = WDTPW | WDT_MDLY_0_5;			/* Set up watchdog to exp, 0.5ms */
+	IE1 |= WDTIE;							/* enable wtd interrupt */
   	
 	/* configure the internal oscillator */
-	BCSCTL1 = CALBC1_1MHZ_;	// set up for 1mhz stable operation without a crystal
+	BCSCTL1 = CALBC1_1MHZ_;	 /* set up for 1mhz stable operation without a crystal */
 	DCOCTL = CALDCO_1MHZ_;
   
 	initIO();
 	/* enable interrupts */
-	//dint(); // opposite is 
+	/* dint(); */ /* opposite is  */
 	eint();
 	
 	/* switch to low power mode, act on interrupts */
-	//LPM0;			
+	/* LPM0; */			
 	
 	while(1)
 	{
@@ -56,15 +66,15 @@ void main(void)
 interrupt(WDT_VECTOR) WTD_interrupt(void)
 {   
 	uint32_t oldTime;
-	// increment the time counter   	
+	/* increment the time counter */   	
 	regTimer++;
-	// update the keys
+	/* update the keys */
 	keyRegOld = keyReg;
 	keyReg = scanKeys();
 	keyPulse = (keyReg ^ keyRegOld) & keyReg;
 
 	
-	// state machine for setting the LED tempo
+	/* state machine for setting the LED tempo */
 	switch(tapState)
 	{
 		case 0:
@@ -90,8 +100,9 @@ interrupt(WDT_VECTOR) WTD_interrupt(void)
 		break;
 	}
 
-	// Control the output channels, if a button is pressed, 
-	// the channel must be bumped.
+	/* Control the output channels, if a button is pressed,
+	 * the channel must be bumped. 
+     */
 	if(keyReg & BIT3)
 	{
 		_setPort(1, EN_CH1);
@@ -136,51 +147,42 @@ void initIO(void)
 	
 	/* configure IO ports */
 			
-	P1SEL = 0x00;		// disable any special functions
+	P1SEL = 0x00;		/* disable any special functions */
 	P2SEL = 0x00;
 	
-	P1DIR = 0x8F;		// set up the port directions
-	P1REN = 0x70;		// enable internal resistors
-	P1OUT = 0x70;		// set resistors to pullup
+	P1DIR = 0x8F;		/* set up the port directions */
+	P1REN = 0x70;		/* enable internal resistors */
+	P1OUT = 0x70;		/* set resistors to pullup */
 	
-	P2DIR = BIT6 ;		// set up port directions
-	P2REN = BIT7;		// enable internal resistors
-	P2OUT = BIT7;		// set resistors to pullup
+	P2DIR = BIT6 ;		/* set up port directions */
+	P2REN = BIT7;		/* enable internal resistors */
+	P2OUT = BIT7;		/* set resistors to pullup */
 	
 	return;
 }
 
 /*
-	Scans the 6 input keys and updates a 8 bit value containing the button statuses
-	BIT0	-	BTN2	-	CH2
-	BIT1	-	BTN4	-	CH4
-	BIT2	-	BTN6	-	STARTSTOP
-	BIT3	-	BTN1	-	CH1
-	BIT4	-	BTN3	-	CH3
-	BIT5	-	BTN5	-	TAP
-	BIT6	-	Reserved
-	BIT7	-	Reserved
+ *  Scans the 6 input keys and updates a 8 bit value containing the button statuses
+ *  BIT0	-	BTN2	-	CH2
+ *  BIT1	-	BTN4	-	CH4
+ *  BIT2	-	BTN6	-	STARTSTOP
+ *  BIT3	-	BTN1	-	CH1
+ *  BIT4	-	BTN3	-	CH3
+ *  BIT5	-	BTN5	-	TAP
+ *  BIT6	-	Reserved
+ *  BIT7	-	Reserved
 */
 uint8_t scanKeys(void)
 {
 	uint8_t keys=0;
-
 	_clearPort(1, BTNC1);
-
 	brief_pause(10000);
-
 	keys = (P1IN & 0x70);
-
 	keys = keys >> 3;
-
 	_setPort(1, BTNC1);
-
 	brief_pause(10000);
-
 	keys |= (P1IN & 0x70);
-
 	keys = keys >> 1;
-
 	return  ((~keys)& 0x3f);
 }
 
@@ -201,3 +203,5 @@ void delayMs(uint32_t delay)
 	while( regTimer < i ) nop();
 	return;
 }
+
+/* vim: set noet :*/
