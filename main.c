a7bdc184f43b77a86c13699d99b9e087de3e6a28
Olivia Theze <fox@foxiepa.ws>
2014-11-28 05:32:48 +0000

    move function prototypes, defines, and macros to a new header

--- main.c
+++ main.c
@@ -9,38 +9,7 @@
 #include <msp430.h>
 #include <signal.h>
 #include <inttypes.h>
-
-/* Function Prototypes */
-uint8_t scanKeys(void);
-void initIO(void);
-static void __inline__ brief_pause(register unsigned int n);
-void delayMs(uint32_t);
-
-/* Definitions */
-#define MODESW_MASK 0xC0
-//port 2 definitions
-#define LED_HEARTBEAT BIT6
-#define BTN_PROGRA	BIT8
-// port 1 definitions
-#define EN_CH1	BIT0
-#define EN_CH2	BIT1
-#define EN_CH3	BIT2
-#define EN_CH4	BIT3
-#define BTNR1	BIT4
-#define BTNR2	BIT5
-#define BTNR3	BIT6
-#define BTNC1	0x80;
-//const int BTNC1 = BIT7;
-
-/* macro to help us toggle bits on ports */
-#define _togglePort(port,bit) P##port##OUT^= bit
-#define _setPort(port,bit) P##port##OUT |= bit
-#define _clearPort(port,bit) P##port##OUT &= ~bit
-
-/* general bit manipulations */
-#define _toggleBit(reg,bit) reg ^= bit  
-#define _setBit(reg,bit) reg |= bit
-#define _clearBit(reg,bit) reg &= ~##bit
+#include "chaser.h"
 
 /* global vars */
 volatile uint32_t regTimer = 0;	// timer register, starts at 0 on startup, 500uS steps
