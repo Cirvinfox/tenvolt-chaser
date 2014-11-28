25184ef50e01812288fd549c58d50423c32049cb
Olivia Theze <fox@foxiepa.ws>
2014-11-28 06:06:07 +0000

    enforcement of style on chaser.h

--- chaser.h
+++ chaser.h
@@ -3,15 +3,12 @@
  *
  *       Filename:  chaser.h
  *
- *    Description:  
+ *    Description:  header file for chaser.c
  *
- *        Version:  1.0
- *        Created:  28/11/14 05:29:54
- *       Revision:  none
- *       Compiler:  gcc
+ *        Changed: 2014-11-28
  *
- *         Author:  Olivia Theze (foxiepaws), fox@foxiepa.ws
- *   Organization:  
+ *         Author:  Corbin Irvin (cirvinfox)
+ *                  Olivia Theze (foxiepaws), fox@foxiepa.ws
  *
  * =====================================================================================
  */
@@ -24,10 +21,10 @@ void delayMs(uint32_t);
 
 /* Definitions */
 #define MODESW_MASK 0xC0
-//port 2 definitions
+/* port 2 definitions */
 #define LED_HEARTBEAT BIT6
 #define BTN_PROGRA	BIT8
-// port 1 definitions
+/* port 1 definitions */
 #define EN_CH1	BIT0
 #define EN_CH2	BIT1
 #define EN_CH3	BIT2
@@ -36,7 +33,7 @@ void delayMs(uint32_t);
 #define BTNR2	BIT5
 #define BTNR3	BIT6
 #define BTNC1	0x80;
-//const int BTNC1 = BIT7;
+/* const int BTNC1 = BIT7; */
 
 /* macro to help us toggle bits on ports */
 #define _togglePort(port,bit) P##port##OUT^= bit
